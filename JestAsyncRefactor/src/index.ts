5223658d5ddfb207d4c57b78f9cb23bed5611fd5
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2024-11-11 20:56:54 +0530

    codemod 3 working

--- JestAsyncRefactor/src/index.ts
+++ JestAsyncRefactor/src/index.ts
@@ -1,19 +1,25 @@
 import type { Api } from "@codemod.com/workflow";
 
-export async function workflow({ files } : Api) {
+export async function workflow({ files }: Api) {
   await files("**/*.ts")
     .jsFam()
-    .astGrep(
-
-rule:
-  kind: expression_statement
-  pattern: "expect(wrapper.instance().$A).toHaveBeenCalled();"
-fix: |
-  await waitFor(() => {
-    expect(MyComponent.prototype.$A).toHaveBeenCalledTimes(1);
-  });
-    )
-    .replace(({getNode, getMatch}) => {
-      console.log(getNode().text())
+    .astGrep({
+      rule: {
+        kind: "expression_statement",
+        pattern: "expect(wrapper.instance().$A).toHaveBeenCalled();"
+      },
+      fix: `
+        await waitFor(() => {
+          expect(MyComponent.prototype.$A).toHaveBeenCalledTimes(1);
+        });
+      `,
+    })
+    .replace(({ getNode, getMatch }) => {
+      console.log(getNode().text());
+      return `
+        await waitFor(() => {
+          expect(MyComponent.prototype.$A).toHaveBeenCalledTimes(1);
+        });
+      `;
     });
 }
